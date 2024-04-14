403e8c1f46e2f0679c52980163d9ca3ef2ad6e0a
njjs1991 <njjs1991@163.com>
2024-04-14 20:31:26 +0800

    周期性更新，详见README

--- js/note.js
+++ js/note.js
@@ -112,6 +112,7 @@ function newText(item, findStr)
 function findStart()
 {
 	document.getElementsByClassName("findblock")[0].style.display = "";
+	document.getElementById("find").select();
 }
 
 function endFind()
